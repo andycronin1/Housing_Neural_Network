0a43800d05e2014a43a081b93f363f8e45b66f01
Andy Cronin <ajcronin@deloitte.co.uk>
2025-06-13 16:10:02 +0100

    updates with data analysis

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,8 +3,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from datetime import date, datetime
 
 from dotenv import load_dotenv
+from pandas.io.sas.sas_constants import column_name_text_subheader_length
+from tensorflow.python.ops.ragged.ragged_embedding_ops import embedding_lookup_sparse
+
 load_dotenv()
 
 BASE_DIR = os.getenv('BASE')
@@ -21,16 +25,59 @@ class DataCleaningPipeline:
         self.data = pd.read_csv(self.data_file_path)
         self.info_data = pd.read_csv(self.data_info_path, index_col='LoanStatNew')
 
-    def feat_info(self, col_name):
-        print(self.info_data.loc[col_name]['Description'])
+    def feat_info(self, *args):
+        for arg in args:
+            print(f' Description of {arg} is: {self.info_data.loc[arg]['Description']}')
 
     def analyse_nulls(self):
         # Analysing nulls as percentage of the dataframe
         print(self.data.isnull().sum())
         print(100* self.data.isnull().sum()/len(self.data))
 
-        filled_data = self.data.dropna()
-        print(f'Number of NaN values dropped = {self.data.shape[0] - filled_data.shape[0]}')
+        # Understanding the data in high percentage nulls columns.
+        self.feat_info('emp_title', 'emp_length')
+        self.feat_info()
+
+        print(f'Number of unique Jobs titles in the dataset = {self.data['emp_title'].nunique()}')
+        # Realistically there are too many unique job titles to try to convert this to a dummy variable feature. So remove the emp_title column.
+        self.data.drop(columns=['emp_title'], inplace=True)
+
+        emp_length_order = ['< 1 year',
+                            '1 year',
+                            '2 years',
+                            '3 years',
+                            '4 years',
+                            '5 years',
+                            '6 years',
+                            '7 years',
+                            '8 years',
+                            '9 years',
+                            '10+ years']
+
+        # sns.countplot(data=self.data, x='emp_length', order=emp_length_order, palette='coolwarm', hue='loan_status')
+        # plt.show()
+
+        # **CHALLENGE TASK: \
+        # This still doesn't really inform us if there is a strong relationship between employment length and being charged off,
+        # what we want is the percentage of charge offs per category.
+        # Essentially informing us what percent of people per employment length category didn't pay back their loan.
+        # There are a multitude of ways to create this Series.
+        # Once you've created it, see if visualize it with a [bar plot](https://pandas.pydata.org/pandas-docs/version/0.23.4/generated/pandas.DataFrame.plot.html).
+        # This may be tricky, refer to solutions if you get stuck on creating this Series.**s
+
+        dummy_data = self.data[['loan_status', 'emp_length']]
+        dummy_data['Numerated'] = [True if x=='Charged Off' else False for x in dummy_data['loan_status']]
+
+        percentage_data = dummy_data.groupby('emp_length')['Numerated'].mean().reset_index()
+        percentage_data.set_index('emp_length', inplace=True)
+        sns.barplot(data=percentage_data, x='emp_length', y='Numerated', order=emp_length_order, palette='coolwarm')
+        plt.show()
+
+
+
+        a=1
+
+
 
     def run(self):
         pass
